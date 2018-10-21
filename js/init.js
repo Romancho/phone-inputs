71c3cd9dfd6f8c3a0add5fa9bc26d142e5ec30c1
Romancho <rpr354@gmail.com>
2018-10-21 21:11:55 +0300

    Фикс бага в мобильной версии

--- js/init.js
+++ js/init.js
@@ -1,31 +1,5 @@
 $(document).ready(function(){
- /* var phone_input = $('#id_phone'),
-      phone_attr = $('#authForm [type="submit"]');
-
-
-  phone_input.keydown(function(){
-      var phone_val = $(this).val();
-
-      if((phone_val === '7') || (phone_val === '8')) {
-          console.log('78' + phone_val);
-          maskInit(phone_input);
-      } else {
-          console.log('Un'+ phone_val);
-      }
-    var $val = phone_input.val(),
-        $val = $val.replace(/[^0-9]/gi, ''),
-        num = $val.match(/[\d\.]+/g);
-        if (num != null) {
-            var number = num.toString().length;
-            if(number === 11) {
-                phone_attr.attr('disabled', false);
-            } else {
-                phone_attr.attr('disabled', true);
-            }
-        } 
-  });*/
-
-    var maskList = $.masksSort($.masksLoad("/js/mask/data/phone-codes.json"), ['#'], /[0-9]|#/, "mask");
+    var maskList = $.masksSort($.masksLoad("/js/libs/mask/data/phone-codes.json"), ['#'], /[0-9]|#/, "mask");
     var maskOpts = {
         inputmask: {
             definitions: {
@@ -34,49 +8,37 @@ $(document).ready(function(){
                     cardinality: 1
                 }
             },
-            //clearIncomplete: true,
             showMaskOnHover: false,
-            autoUnmask: true
+            autoUnmask: true,
+            clearMaskOnLostFocus: false
         },
         match: /[0-9]/,
         replace: '#',
         list: maskList,
         listKey: "mask",
-        onMaskChange: function(maskObj, completed) {
-            if (completed) {
-                var hint = maskObj.name_ru;
-                if (maskObj.desc_ru && maskObj.desc_ru != "") {
-                    hint += " (" + maskObj.desc_ru + ")";
+        onMaskChange: function(maskObj, determined) {
+            if (determined) {
+                var hint = maskObj.name_en;
+                if (maskObj.desc_en && maskObj.desc_en != "") {
+                    hint += " (" + maskObj.desc_en + ")";
                 }
                 $("#descr").html(hint);
             } else {
-                $("#descr").html("Маска ввода");
+                $("#descr").html("Mask of input");
             }
-            $(this).attr("placeholder", $(this).inputmask("getemptymask"));
         }
     };
 
     $('#phone_mask').change(function() {
         if ($('#phone_mask').is(':checked')) {
+            $('#customer_phone').inputmask("remove");
             $('#customer_phone').inputmasks(maskOpts);
         } else {
-            $('#customer_phone').inputmask("+[####################]", maskOpts.inputmask)
-                .attr("placeholder", $('#customer_phone').inputmask("getemptymask"));
-            $("#descr").html("Маска ввода");
+            $('#customer_phone').inputmasks("remove");
+            $('#customer_phone').inputmask("+#{*}", maskOpts.inputmask);
+            $("#descr").html("Mask of input");
         }
     });
 
     $('#phone_mask').change();
 });
-function maskInit(phone_input) {
-    phone_input.mask("+9 (999) 999-9999");
-    phone_input.on("blur", function() {
-        var last = $(this).val().substr( $(this).val().indexOf("-") + 1 );
-        if( last.length == 5 ) {
-            var move = $(this).val().substr( $(this).val().indexOf("-") + 1, 1 );
-            var lastfour = last.substr(1,4);
-            var first = $(this).val().substr( 0, 9 );
-            $(this).val( first + move + '-' + lastfour );
-        }
-    });
-}
